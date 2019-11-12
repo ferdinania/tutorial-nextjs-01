e7ba82d18ad75831d47b2dfeefbcd3c382169589
Andres Fernando <andres.fernando08@gmail.com>
2019-11-11 22:36:55 -0500

    css in js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
 import Link from 'next/link'
 import Layout from '../comps/Layout'
 
+function getPosts() {
+    return [
+        { id: 'hello-next-js', title: 'Hello Next.js' },
+        { id: 'learning-next-js-is-awesome', title: 'Learning Next.js is awesome' },
+        { id: 'deploy-next-js', title: 'Deploy Next.js App' },
+    ]
+}
+
 const Index = () => (
     <Layout>
         <p>Hello Next.js</p>
         <ul>
-            <li>
-                <Link href='/post?slug=anime-mas' as='/post/anime-mas'>Anime y mas</Link>
-            </li>
-            <li>
-                <Link href='/post?slug=conociendo-next-js' as='/post/conociendo-next-js'>Conociendo Next.js</Link>
-            </li>
-            <li>
-                <Link href='/post?slug=routing-pages' as='/post/routing-pages'>Routing pages</Link>
-            </li>
+            {getPosts().map(post => (
+                <li key={post.id}>
+                    <Link href='/a/[id]' as={`/a/${post.id}`}>
+                        <a>{post.title}</a>
+                    </Link>
+                </li>
+            ))}
+            <style jsx>{`
+                h1,
+                a {
+                    font-family: 'Arial';
+                }
+                ul {
+                    padding: 0;
+                }
+                li {
+                    list-style: none;
+                    margin: 5px 0;
+                }
+                a {
+                    text-decoration: none;
+                    color: blue;
+                }
+                a:hover {
+                    opacity: 0.6;
+                }
+            `}</style>
         </ul>
     </Layout>
 )
